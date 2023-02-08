0c89c535d4a4b439d3e870da2c0c5459619dff33
alexm <alex-m@outlook.com>
2023-02-08 15:20:16 -0800

    Edit module docstring; set up Flask debug toolbar

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 # Unit 23 Exercise - Blogly App
 
 """
-Blogly application.
+Blogly application - Flask setup and routes.
 """
 
 from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
 
@@ -15,6 +16,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+app.config["SECRET_KEY"] = "O secreta foarte secreta"
+debug = DebugToolbarExtension(app)
 
 with app.app_context():
     db.create_all()
