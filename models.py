131ee14da62d1a48ae5eeee2236be75c0e6ea21b
alexm <alex-m@outlook.com>
2023-02-08 15:38:52 -0800

    Write model for users table

--- models.py
+++ models.py
@@ -17,3 +17,20 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class User(db.Model):
+    """
+    Class representing a user.
+    """
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    image_url = db.Column(db.Text)
+
