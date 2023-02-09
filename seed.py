b7630518b5eb9b0fce385779083ca43daec9329a
alexm <alex-m@outlook.com>
2023-02-09 15:05:56 -0800

    Wrap all seed code in application context

--- seed.py
+++ seed.py
@@ -13,14 +13,13 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-# If table isn't empty, empty it
-User.query.delete()
+    User.query.delete()
 
-# Add users
-jane = User(first_name="Jane", last_name="Doe")
-xavier = User(first_name="Xavier", last_name="Xavies", image_url="dummylink")
-ally = User(first_name="Ally", last_name="Andrews", image_url="dummylink2")
+    # Add users
+    jane = User(first_name="Jane", last_name="Doe")
+    xavier = User(first_name="Xavier", last_name="Xavies", image_url="dummylink")
+    ally = User(first_name="Ally", last_name="Andrews", image_url="dummylink2")
 
-# Add users to session and commit
-db.session.add_all([jane, xavier, ally])
-db.session.commit()
+    # Add users to session and commit
+    db.session.add_all([jane, xavier, ally])
+    db.session.commit()
