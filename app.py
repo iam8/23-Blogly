618e48d10764642a62c84edf0ec3d25c060e7568
alexm <alex-m@outlook.com>
2023-02-08 16:37:32 -0800

    Add route for homepage; add code to run Flask app on 0.0.0.0 (localhost currently not working)

--- app.py
+++ app.py
@@ -9,10 +9,8 @@ from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
@@ -22,3 +20,17 @@ debug = DebugToolbarExtension(app)
 
 with app.app_context():
     db.create_all()
+
+
+@app.route("/")
+def homepage():
+    """
+    Blogly app homepage - shows list of current users and an 'add user' button.
+    """
+
+    users = User.query.all()
+    return render_template("home.jinja2", users=users)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
