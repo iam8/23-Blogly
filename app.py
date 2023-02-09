165030ea2cde71426fd0520326cfcf2100e45e97
alexm <alex-m@outlook.com>
2023-02-08 17:05:21 -0800

    Add route URLs and view function stubs and docstrings for all remaining required routes for application

--- app.py
+++ app.py
@@ -42,5 +42,47 @@ def list_users():
     return render_template("home.jinja2", users=users)
 
 
+@app.route("/users/new")
+def display_add_user_form():
+    """
+    Display form to add a new user to Blogly app database.
+    """
+
+
+@app.route("/users/new", methods=["POST"])
+def add_user():
+    """
+    Add new user to Blogly app database and redirect to /users (user list) page.
+    """
+
+
+@app.route("/users/<int:user_id>")
+def display_user_details(user_id):
+    """
+    Show information about the user with the given ID, and buttons to edit or delete that user.
+    """
+
+
+@app.route("/users/<int:user_id>/edit")
+def display_edit_form(user_id):
+    """
+    Display the edit page for the user with the given ID.
+    """
+
+
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
+def edit_user(user_id):
+    """
+    Edit the user with the given ID and redirect to /users (user list) page.
+    """
+
+
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    """
+    Delete the user with the given ID and redirect to /users (user list) page.
+    """
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
