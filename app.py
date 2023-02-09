c7f02196a9d19fe7a9d01ed1ef3efe5840132bf7
alexm <alex-m@outlook.com>
2023-02-08 21:39:47 -0800

    Display edit form view: retrieve user with given ID and render template with edit form

--- app.py
+++ app.py
@@ -87,7 +87,8 @@ def display_edit_form(user_id):
     Display the edit page for the user with the given ID.
     """
 
-    return f"Edit page goes here: {user_id}"
+    user = User.query.get(user_id)
+    return render_template("edit_user.jinja2", user=user)
 
 
 @app.route("/users/<int:user_id>/edit", methods=["POST"])
