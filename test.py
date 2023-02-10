4c5b117a8f1f71fe2fb2d46a623966d650456bc4
alexm <alex-m@outlook.com>
2023-02-09 16:17:02 -0800

    Write tests for getting user list and for displaying new user form

--- test.py
+++ test.py
@@ -41,3 +41,32 @@ class FlaskTests(TestCase):
             self.assertEqual(resp.status_code, 200)
             self.assertIn("<h1>Users</h1>", html)
             self.assertIn("<button>Add user</button>", html)
+
+    def test_get_user_list(self):
+        """
+        Test that GET '/users' results in a status code of 200 and returns a page with the
+        appropriate content.
+        """
+
+        with app.test_client() as client:
+            resp = client.get("/users")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("<h1>Users</h1>", html)
+            self.assertIn("<button>Add user</button>", html)
+
+    def test_get_new_user_form(self):
+        """
+        Test that GET '/users/new' results in a status code of 200 and returns a page with the
+        appropriate content.
+        """
+
+        with app.test_client() as client:
+            resp = client.get("/users/new")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("<h1>Create A New User</h1>", html)
+            self.assertIn("<button>Add</button>", html)
+            self.assertRegex(html, '<form .* method="POST">')
