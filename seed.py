e072bf205bacaacc7a5f3de525c646244e89ef07
alexm <alex-m@outlook.com>
2023-02-09 21:11:00 -0800

    Update seed file with initial posts data; connect to DB; place all code inside 'if name equals main' block

--- seed.py
+++ seed.py
@@ -2,24 +2,41 @@
 # Unit 23 Exercise - Blogly App
 
 """
-Seed file for Blogly app - add 3 initial users into database.
+Seed file for Blogly app - add 3 initial users and some posts into database.
 """
 
-from models import User, db
-from app import app
+from models import User, Post, db
+from app import app, connect_db
 
-# Drop and create all tables
-with app.app_context():
-    db.drop_all()
-    db.create_all()
+if __name__ == "__main__":
 
-    User.query.delete()
+    connect_db(app)
 
-    # Add users
-    jane = User(first_name="Jane", last_name="Doe")
-    xavier = User(first_name="Xavier", last_name="Xavies", image_url="dummylink")
-    ally = User(first_name="Ally", last_name="Andrews", image_url="dummylink2")
+    # Drop and create all tables
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
 
-    # Add users to session and commit
-    db.session.add_all([jane, xavier, ally])
-    db.session.commit()
+        Post.query.delete()
+        User.query.delete()
+
+        # Create users
+        jane = User(first_name="Jane", last_name="Doe")
+        xavier = User(first_name="Xavier", last_name="Xavies", image_url="dummylink")
+        ally = User(first_name="Ally", last_name="Andrews", image_url="dummylink2")
+
+        db.session.add_all([jane, xavier, ally])
+        db.session.commit()
+
+        # Create some posts
+        post_jane = Post(title="Jane's post",
+                         content="This is a post by Jane",
+                         user_id=jane.id)
+
+        post_jane2 = Post(title="Jane's post 2",
+                          content="This is another post by Jane",
+                          user_id=jane.id)
+
+        db.session.add_all([post_jane, post_jane2])
+        db.session.commit()
+
