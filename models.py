7ab40dbe125ac825268176557979feab8bdf8731
alexm <alex-m@outlook.com>
2023-02-12 19:13:26 -0800

    Fix typo in posts and tags table names in PostTag foreign key declarations

--- models.py
+++ models.py
@@ -116,5 +116,5 @@ class PostTag(db.Model):
 
     __tablename__ = "posts_tags"
 
-    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey("tag.id"), primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), primary_key=True)
